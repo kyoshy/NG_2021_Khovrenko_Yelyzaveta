33628ae664479ba9a467e08a5e80b33b928e1b3f
kyoshy <x-e-d@ukr.net>
2021-07-09 20:13:15 +0300

    Added Task_2 to Lesson_1

---
+++ Lesson1/Task2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter the first number:";
+    int first = 0, second = 0;
+    cin >> first;
+    cout << "Enter the second number:";
+    cin >> second;
+    cout << "The sum: " << first + second;
+    return 0;
+}
