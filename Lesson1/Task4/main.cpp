cdf8731edea147d827cb10e03338396fe292e24f
kyoshy <x-e-d@ukr.net>
2021-07-11 16:20:49 +0300

    Updated Task 4 from Lesson 1

--- Lesson1/Task4/main.cpp
+++ Lesson1/Task4/main.cpp
@@ -7,17 +7,18 @@ int main()
     cout << "Enter your slary: " << endl;
     int pay = 0;
     cin >> pay;
-    if (pay<1000000)
+    if (pay>1000)
     {
-        if (pay>1000)
+        if (pay<1000000)
         {
             cout << "Cool";
         }
-        if (pay<1001)
-        {
-            cout << "Work harder";
-        }
     }
+    if (pay<1001)
+    {
+        cout << "Work harder";
+    }
+
     if (pay>999999)
     {
         cout << "You are millionaire";
