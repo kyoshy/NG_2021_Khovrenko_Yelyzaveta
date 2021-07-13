e8d37eb5bf196725fa3294e2c25485c59bf15b5c
kyoshy <x-e-d@ukr.net>
2021-07-13 23:38:45 +0300

    Updated Task 2 from Lesson 2 - corrected the style of code

--- Lesson2/Task2/main.cpp
+++ Lesson2/Task2/main.cpp
@@ -7,8 +7,8 @@ int main()
     int number = 0;
     do
     {
-    cout << "Enter the positive number: ";
-    cin >> number;
+        cout << "Enter the positive number: ";
+        cin >> number;
     }while (number < 0);
     for (int i = 0; i <= number; i++)
     {
