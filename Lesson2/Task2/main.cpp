679fff7f8505d0e6bb5ccec5fe488b6794a16246
kyoshy <x-e-d@ukr.net>
2021-07-13 23:24:07 +0300

    Added Task 2 to Lesson 2

---
+++ Lesson2/Task2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    do
+    {
+    cout << "Enter the positive number: ";
+    cin >> number;
+    }while (number < 0);
+    for (int i = 0; i <= number; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
