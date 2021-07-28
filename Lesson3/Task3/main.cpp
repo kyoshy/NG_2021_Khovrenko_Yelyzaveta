56078820d9b757097b7532a805565ccb962877d7
kyoshy <emmaflyp@gmail.com>
2021-07-28 18:31:12 +0300

    Updated Task 3 from Lesson 3 - the values entered by the user do not change

--- Lesson3/Task3/main.cpp
+++ Lesson3/Task3/main.cpp
@@ -20,16 +20,31 @@ int main()
             }while (numbers[i] < 0);
         }
     }
-    int stop = 0;
-    while (stop < 5)
+
+     // search for the largest number in the array
+    int largest = 0;
+    for (int big = 0; big < 20; big++)
+    {
+        int high = largest;
+        if (numbers[big] == 0)
+            break;
+        if (numbers[big+1] < numbers[big])
+            high = numbers[big];
+        if (high > largest)
+            largest = high;
+    }
+
+    int arr[5] = {0, 0, 0, 0, 0};
+
+    for (int stop = 0; stop < largest; stop++)
     {
         cout << " ";
         for (int column = 0; column < 5; column++)
         {
-            if (numbers[column] > 0)
+            if (numbers[column] != 0 && numbers[column] > arr[column])
             {
                 cout << "*";
-                numbers[column]--;
+                arr[column]++;
             }
             else
             {
@@ -37,12 +52,6 @@ int main()
             }
         }
         cout << endl;
-        stop = 0;
-        for (int i = 0; i < 5; i++)
-        {
-            if (numbers[i] == 0)
-                stop++;
-        }
     }
     return 0;
 }
