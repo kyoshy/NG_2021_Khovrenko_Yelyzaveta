cfc25404dd2a1a8d979568bcd033a92cc85eb80e
kyoshy <emmaflyp@gmail.com>
2021-08-03 16:31:26 +0300

    Updated Task 3 from Lesson 3 - only one array - no search for the largest number in the array

--- Lesson3/Task3/main.cpp
+++ Lesson3/Task3/main.cpp
@@ -10,48 +10,34 @@ int main()
     {
         cout << i+1 << ") ";
         cin >> numbers[i];
-        if (numbers[i] < 0)
+        while (numbers[i] < 0)
         {
-            do
-            {
-                cout << "The number should be positive! Try again" << endl;
-                cout << i+1 << ") ";
-                cin >> numbers[i];
-            }while (numbers[i] < 0);
+            cout << "The number should be positive! Try again" << endl;
+            cout << i+1 << ") ";
+            cin >> numbers[i];
         }
     }
 
-     // search for the largest number in the array
-    int largest = 0;
-    for (int big = 0; big < 20; big++)
+    int row = 0;
+    int stop = 0;
+    while (stop < 5)
     {
-        int high = largest;
-        if (numbers[big] == 0)
-            break;
-        if (numbers[big+1] < numbers[big])
-            high = numbers[big];
-        if (high > largest)
-            largest = high;
-    }
-
-    int arr[5] = {0, 0, 0, 0, 0};
-
-    for (int stop = 0; stop < largest; stop++)
-    {
+        stop = 0;
         cout << " ";
         for (int column = 0; column < 5; column++)
         {
-            if (numbers[column] != 0 && numbers[column] > arr[column])
+            if (numbers[column] != 0 && numbers[column] > row)
             {
                 cout << "*";
-                arr[column]++;
             }
             else
             {
                 cout << " ";
+                stop++;
             }
         }
         cout << endl;
+        row++;
     }
     return 0;
 }
