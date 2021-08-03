12371c2d5cab0b1103da3b2b94320330834dbbfc
kyoshy <emmaflyp@gmail.com>
2021-08-03 16:05:43 +0300

    Updated Task 2 from Lesson 3 - fixed the calculation of the amount of money on the cards

--- Lesson3/Task2/main.cpp
+++ Lesson3/Task2/main.cpp
@@ -7,12 +7,14 @@ int main()
     int cards[10];
     int command = 0;
     int card = 0;
-    int money = 0;
     int summa = 0;
+    int money = 0;
+
     for (int i = 0; i < 10; i++)
     {
         cards[i] = 0;
     }
+
     do
     {
         cout << "YOUR CARDS" << endl;
@@ -31,7 +33,7 @@ int main()
             cout << "How much money: ";
             cin >> money;
         }while (money < 0);
-        cards[card-1] = money;
+        cards[card-1] += money;
         summa += money;
         cout << "BALANCE: " << summa << endl;
         cout << "Enter 0 to continue or 1 to exit: ";
