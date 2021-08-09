bdbb49c7c0a9f2603373e2e37c8edaa1b9e13a36
kyoshy <emmaflyp@gmail.com>
2021-08-09 17:03:31 +0300

    Updated Task 1 from Lesson 4 - less arrays

--- Lesson4/Task1/main.cpp
+++ Lesson4/Task1/main.cpp
@@ -9,23 +9,11 @@ int main()
     cin.getline(str, 100);
     int words = 0;
     int i = 0;
-    char letters[] = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g',
-                     'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n',
-                     'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u',
-                     'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'};
-    int same[100];
 
-    for (int filling = 0; filling < 100; filling++)
-        same[filling] = 0;
-
-    while (str[i] != 0)
+    while (str[i] != '\0')
     {
-        for (int compare = 0; compare < 52; compare++)
-        {
-            if (str[i] == letters[compare])
-               same[i]++;
-        }
-        if (same[i] == 0 && same[i-1] == 1 || str[i+1] == 0)
+        if((str[i] < 'A' || str[i] > 'Z' && str[i] < 'a' || str[i] > 'z') &&
+           (str[i-1] >= 'A' && str[i-1] <= 'Z' || str[i-1] >= 'a' && str[i-1] <= 'z') || str[i+1] == '\0')
             words++;
         i++;
     }
@@ -41,5 +29,6 @@ int main()
     default:
         cout << "There are " << words << " words in the string.";
     }
+    cout << endl << endl;
     return 0;
 }
