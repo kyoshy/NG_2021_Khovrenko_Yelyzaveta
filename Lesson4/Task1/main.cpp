f839b769a2f8e573c3cf9fe542942b00048977c2
kyoshy <emmaflyp@gmail.com>
2021-08-10 13:41:59 +0300

    Updated Task 1 from Lesson 4 - fixed algorithm - code is simplified

--- Lesson4/Task1/main.cpp
+++ Lesson4/Task1/main.cpp
@@ -8,14 +8,12 @@ int main()
     cout << "Enter your string:" << endl;
     cin.getline(str, 100);
     int words = 0;
-    int i = 0;
 
-    while (str[i] != '\0')
+    for (int i = 0; str[i] != '\0'; i++)
     {
         if((str[i] < 'A' || str[i] > 'Z' && str[i] < 'a' || str[i] > 'z') &&
-           (str[i-1] >= 'A' && str[i-1] <= 'Z' || str[i-1] >= 'a' && str[i-1] <= 'z') || str[i+1] == '\0')
+           (str[i+1] >= 'A' && str[i+1] <= 'Z' || str[i+1] >= 'a' && str[i+1] <= 'z') || str[i+1] == '\0')
             words++;
-        i++;
     }
 
     switch (words)
@@ -29,6 +27,5 @@ int main()
     default:
         cout << "There are " << words << " words in the string.";
     }
-    cout << endl << endl;
     return 0;
 }
