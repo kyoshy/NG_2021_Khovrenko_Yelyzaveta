fee6923ebff11a6670e16883d2a7799803569fcc
kyoshy <emmaflyp@gmail.com>
2021-08-18 00:22:55 +0300

    Updated Task 3 from Lesson 4 - less variables

--- Lesson4/Task3/main.cpp
+++ Lesson4/Task3/main.cpp
@@ -4,16 +4,15 @@ using namespace std;
 
 int main()
 {
-    char str[100], big[100];
+    char str[256];
     cout << "Enter your string:" << endl;
-    cin.getline(str, 100);
+    cin.getline(str, 256);
 
     for (int i = 0; str[i] != '\0'; i++)
     {
         if (str[i] >= 'a' && str[i] <= 'z')
         {
-            big[i] = str[i] - 32;
-            cout << big[i];
+            cout << (char)(str[i]-32);
         }
         else
             cout << str[i];
