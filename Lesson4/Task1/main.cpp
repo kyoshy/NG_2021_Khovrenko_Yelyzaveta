2f312aa580f169ff78a1bbe2f7834f46c74bfee9
kyoshy <emmaflyp@gmail.com>
2021-09-03 22:42:31 +0300

    Updated Task 1 from Lesson 4 - no switch - numbers are not like words

--- Lesson4/Task1/main.cpp
+++ Lesson4/Task1/main.cpp
@@ -11,21 +11,11 @@ int main()
 
     for (int i = 0; str[i] != '\0'; i++)
     {
-        if((str[i] < 'A' || str[i] > 'Z' && str[i] < 'a' || str[i] > 'z') &&
-           (str[i+1] >= 'A' && str[i+1] <= 'Z' || str[i+1] >= 'a' && str[i+1] <= 'z') || str[i+1] == '\0')
+        if(!(str[i+1] >= 'A' && str[i+1] <= 'Z' || str[i+1] >= 'a' && str[i+1] <= 'z') &&
+           (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z'))
             words++;
     }
 
-    switch (words)
-    {
-    case 0:
-        cout << "There are no words in the string.";
-        break;
-    case 1:
-        cout << "There is 1 word in the string.";
-        break;
-    default:
-        cout << "There are " << words << " words in the string.";
-    }
+    cout << "Words in the string: " << words;
     return 0;
 }
